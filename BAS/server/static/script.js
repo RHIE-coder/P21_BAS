0071757115ee7e008bee39b918bbb86ee9440370
rhie-coder <quotia72@naver.com>
2021-05-24 16:45:23 +0900

    change view performanceCheck.go

--- BAS/server/static/script.js
+++ BAS/server/static/script.js
@@ -19,34 +19,58 @@ function loadJSON(location, callback) {
 }
 
 function showJsonDOM(attrList, data){
+
+  let classificationByClientName = []
+
   for(let i = 0; i < data.length; i++){
-    let itemElement = document.createElement("div");
-    itemElement.setAttribute("class","content-items");
-    for(const attrName of attrList){
-      let section = document.createElement("section");
-      let div = document.createElement("div");
-      div.innerText = attrName;
-      let p = document.createElement("p");
-      p.innerText = data[i][attrName];
-      section.appendChild(div); 
-      section.appendChild(p);
-      itemElement.appendChild(section);
-      contentList.appendChild(itemElement);
+    let clientName = data[i]["DID_client"];
+    if(classificationByClientName.includes(clientName)){
+      continue;
+    }else{
+      classificationByClientName.push(clientName);
     }
   }
+
+  classificationByClientName.forEach(function(clientName){
+    let clientNameElement = document.createElement("div");
+    clientNameElement.setAttribute("class","client-name");
+    clientNameElement.innerText = clientName;
+    contentList.appendChild(clientNameElement);
+
+    for(let i = 0; i < data.length; i++){
+      if(data[i]["DID_client"] == clientName){
+        let itemElement = document.createElement("div");
+        itemElement.setAttribute("class","content-items");
+    
+        for(const attrName of attrList){
+          let section = document.createElement("section");
+          let div = document.createElement("div");
+          div.innerText = attrName;
+          let p = document.createElement("p");
+          p.innerText = data[i][attrName];
+          section.appendChild(div); 
+          section.appendChild(p);
+          itemElement.appendChild(section);
+          contentList.appendChild(itemElement);
+        }
+      }
+    }
+
+  })
 
 }
 
 codeButton.addEventListener('click',function(){
   contentTitle.textContent = "CODE INFORMATION"
   contentList.innerHTML= null
-  
+  // loadJSON('./static/testCode.json',function(data){ //test
   loadJSON('http://localhost:5000/code',function(data){
     let attrList = [
       "InfoType", "ID_code", "DID_RO","DID_client","Scope",
       "Hash_code","Time_issueed","URI_Redirection","Condition","ID_token"];
     
-    showJsonDOM(attrList, JSON.parse(data))
+    showJsonDOM(attrList, JSON.parse(data));
+    // showJsonDOM(attrList, data); //test
   })
 })
 
@@ -59,6 +83,6 @@ tokenButton.addEventListener('click',function(){
       "InfoType","ID_token","DID_RO","DID_client","Scope",
       "Hash_code","Time_issueed","Time_expiration","URI_Redirection","Condition"];
     
-    showJsonDOM(attrList, JSON.parse(data))
+    showJsonDOM(attrList, JSON.parse(data));
   })
 })
